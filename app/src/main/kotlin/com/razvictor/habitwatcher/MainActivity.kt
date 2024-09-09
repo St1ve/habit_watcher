9be8e97db3594343e48c95dab97fc21b0167c99d
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-09-09 22:33:27 +0300

    Add theme

--- app/src/main/kotlin/com/razvictor/habitwatcher/MainActivity.kt
+++ app/src/main/kotlin/com/razvictor/habitwatcher/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.razvictor.habitwatcher.ui.theme.HabitWatcherTheme
+import com.example.compose.HabitWatcherTheme
 
 class MainActivity: ComponentActivity() {
 
