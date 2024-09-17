8a2cc2ff747a4db7a71dc58d4c2198e159d3c0be
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-09-17 21:37:28 +0300

    Add kotlin serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -62,6 +63,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(libs.junit)
 
--- app/src/main/kotlin/com/razvictor/habitwatcher/MainActivity.kt
+++ app/src/main/kotlin/com/razvictor/habitwatcher/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.razvictor.habitwatcher.uikit.HabitWatcherTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity: ComponentActivity() {
 
